fd57266e73db7ec274c0cad9792c84c7d3f545e4
Bryce Tsuyuki <btsuyuki1@gmail.com>
2021-06-06 19:07:01 -0700

    first split works

---
+++ scripts/split.js
@@ -0,0 +1,33 @@
+// Instantiate the SDK. CDN will expose splitio globally
+let myKey = Math.random().toString(36).substring(2);
+// console.log(myKey);
+
+var factory = splitio({
+  core: {
+    authorizationKey: 'vrjk97huv9pf95if78uo9178buskbfnbi23t',
+    // your internal user id, or the account id that
+    // the user belongs to.
+    // This coudld also be a cookie you generate
+    // for anonymous users
+    // random generation from https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript but with a different length
+    key: myKey,
+  },
+});
+// And get the client instance you'll use
+var client = factory.client();
+
+// console.log(factory.client());
+
+client.on(client.Event.SDK_READY, function () {
+  var treatment = client.getTreatment('double_column');
+  //   console.log(treatment);
+  if (treatment == 'on') {
+    document.querySelectorAll('main')[0].className = 'double-column';
+    // console.log('double on');
+  } else if (treatment == 'off') {
+    document.querySelectorAll('main')[0].className = '';
+    // console.log('double off');
+  } else {
+    // insert code for control treatment
+  }
+});
